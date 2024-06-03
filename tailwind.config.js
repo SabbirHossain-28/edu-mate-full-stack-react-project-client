36f99bcf96249cc1c2cfd7ad97e3d6009bf32ce4
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-06-03 13:15:47 +0600

    create custome font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        "raleWay":"'Raleway', 'sans-serif'",
+        "poppin":"'Poppins', 'sans-serif'",
+        "pop":"'Mochiy Pop One', 'sans-serif'",
+      }
+    },
   },
   plugins: [
     require('daisyui'),
