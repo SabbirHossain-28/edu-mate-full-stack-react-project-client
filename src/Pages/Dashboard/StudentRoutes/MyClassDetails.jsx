7be0600ffb7ed53b551b3e676d42785b134dbf2b
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-06-12 11:29:35 +0600

    implement feedback modal functionalities

--- src/Pages/Dashboard/StudentRoutes/MyClassDetails.jsx
+++ src/Pages/Dashboard/StudentRoutes/MyClassDetails.jsx
@@ -9,12 +9,25 @@ import Swal from "sweetalert2";
 import Loading from "../../../Shared/Loading/Loading";
 import SectionHeader from "../../../Shared/SectionHeader/SectionHeader";
 import { IoMdDoneAll } from "react-icons/io";
+import { FaRegHandPointLeft } from "react-icons/fa6";
+import TERModal from "../../../Components/DashboardComponent/Modal/TERModal";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 const MyClassDetails = () => {
   const { id } = useParams();
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
-  //   const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [userRating, setUserRating] = useState(0);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
 
   const { data: submittedAssignmentData = [] } = useQuery({
     queryKey: ["submittedAssignmentData"],
@@ -23,8 +36,6 @@ const MyClassDetails = () => {
       return res.data;
     },
   });
-
-  console.log(submittedAssignmentData);
 
   const { data: classData = {}, isLoading: loadingId } = useQuery({
     queryKey: ["classId", id],
@@ -81,6 +92,53 @@ const MyClassDetails = () => {
     });
   };
 
+  const { mutateAsync: mutateFeedback } = useMutation({
+    mutationFn: async (userFeedback) => {
+      const res = await axiosSecure.post("/feedbacks", userFeedback);
+      return res.data;
+    },
+  });
+
+  const onSubmit = async (data) => {
+    setLoading(true);
+    const userFeedback = {
+      feedback: data?.feedback,
+      rating: userRating,
+      userName: user?.displayName,
+      userEmail: user?.email,
+      userImage: user?.photoURL,
+    };
+    await mutateFeedback(userFeedback, {
+      onSuccess: (data) => {
+        if (data.insertedId) {
+          Swal.fire({
+            title: "Successfully Submitted feedback!",
+            text: "Your valuable feedback is submitted successfully",
+            icon: "success",
+          });
+          setLoading(false);
+          setUserRating(0)
+          handleModalClose();
+        }
+      },
+      onError: (error) => {
+        console.error("Error submitting feedback", error);
+        Swal.fire(
+          "Error",
+          "There was an error submitting your feedback",
+          "error"
+        );
+      },
+    });
+  };
+
+  const handleModalOpen = () => {
+    setIsModalOpen(true);
+  };
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+
   if (loadingId) {
     return (
       <div className="min-h-screen flex justify-center items-center">
@@ -99,6 +157,15 @@ const MyClassDetails = () => {
                 "Submit this assignment before the deadline is over..Please ensure that your submissions adhere to the given guidelines and deadlines. Remember to include all necessary files and information to avoid any delays in grading. We look forward to seeing your creative work and wish you the best of luck! "
               }
             ></SectionHeader>
+          </div>
+          <div className="flex items-center gap-2 mb-4">
+            <button
+              onClick={handleModalOpen}
+              className="text-xl font-pop p-2 border-2 border-base-green bg-base-orange rounded-xl hover:scale-95 hover:transition-all hover:duration-300 hover:ease-in hover:bg-base-green hover:text-white hover:border-base-orange"
+            >
+              Teaching Evaluation Report
+            </button>
+            <FaRegHandPointLeft className="text-3xl text-base-orange"></FaRegHandPointLeft>
           </div>
           {allAssignments.length === 0 ? (
             <p className="text-3xl font-semibold font-raleWay text-center text-base-orange">
@@ -129,7 +196,8 @@ const MyClassDetails = () => {
                             submittedData?.assignmentId === data?._id
                         ) ? (
                           <p className="flex items-center font-semibold">
-                            <IoMdDoneAll className="text-lg"></IoMdDoneAll>Submitted
+                            <IoMdDoneAll className="text-lg"></IoMdDoneAll>
+                            Submitted
                           </p>
                         ) : (
                           <button
@@ -147,6 +215,19 @@ const MyClassDetails = () => {
             </div>
           )}
         </div>
+        {isModalOpen && (
+          <TERModal
+            handleModalClose={handleModalClose}
+            handleSubmit={handleSubmit}
+            onSubmit={onSubmit}
+            register={register}
+            errors={errors}
+            loading={loading}
+            reset={reset}
+            setUserRating={setUserRating}
+            userRating={userRating}
+          ></TERModal>
+        )}
       </Container>
     </div>
   );
