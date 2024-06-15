88bc8f053ee0ce9fa3eb401eb105a521eb02a334
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-06-16 02:18:38 +0600

    implement pagination on tacher dashborad

--- src/Pages/Dashboard/TeacherRoutes/MyClass/MyClass.jsx
+++ src/Pages/Dashboard/TeacherRoutes/MyClass/MyClass.jsx
@@ -15,7 +15,7 @@ import { Link } from "react-router-dom";
 import useAxiosSecure from "../../../../Hooks/useAxiosSecure";
 
 const MyClass = () => {
-  const { user } = useAuth();
+  const { user,loading:userLoading } = useAuth();
   // const axiosCommon = useAxiosCommon();
   const axiosSecure = useAxiosSecure();
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -23,6 +23,7 @@ const MyClass = () => {
   const [classImage, setClassImage] = useState("");
   const [isImageChanged, setIsImageChanged] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
   const {
     register,
     handleSubmit,
@@ -35,14 +36,34 @@ const MyClass = () => {
     refetch,
     isLoading,
   } = useQuery({
-    queryKey: ["requestedClasses"],
-    enabled: !loading && !!user?.email,
+    queryKey: ["requestedClasses",user?.email,currentPage],
+    enabled: !userLoading && !!user?.email,
     queryFn: async () => {
-      const res = await axiosSecure.get(`/classes/${user?.email}`);
+      const res = await axiosSecure.get(`/classes/${user?.email}?page=${currentPage}&size=${10}`);
       return res.data;
     },
-    // enabled: !!user?.email,
   });
+
+  const { data: count = 0 } = useQuery({
+    queryKey: ["count",user?.email],
+    enabled:!userLoading && !!user?.email,
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/teacherCountedClass/${user?.email}`);
+      return res.data.result;
+    },
+  });
+  console.log(count);
+
+  const numberOfPages = Math.ceil(count / 10);
+  const pages = [
+    ...Array(numberOfPages)
+      .keys()
+      .map((key) => key + 1),
+  ];
+
+  const handleCurrentPage = (value) => {
+    setCurrentPage(value);
+  };
 
   const { mutateAsync: mutateDelete } = useMutation({
     mutationFn: async (id) => {
@@ -52,7 +73,6 @@ const MyClass = () => {
   });
 
   const handleClassDelete = (id, classTitle) => {
-    console.log(id);
     Swal.fire({
       title: "Are you sure?",
       text: "Your added class will be delete permanently!",
@@ -105,7 +125,6 @@ const MyClass = () => {
     };
   }, [classImage, isImageChanged]);
 
-  console.log(selectedClass?._id);
   const { mutateAsync: mutateUpdate } = useMutation({
     mutationFn: async (updateClassData) => {
       const res = await axiosSecure.patch(
@@ -192,7 +211,7 @@ const MyClass = () => {
     <div className="bg-slate-200 min-h-screen my-auto">
       <Container>
         <div className="pt-16">
-          <div className="overflow-x-auto bg-base-green border-2 border-black">
+          <div className="overflow-x-auto bg-base-green border-2 border-black p-2">
             <table className="table">
               <thead className="text-gray-300">
                 <tr>
@@ -229,7 +248,13 @@ const MyClass = () => {
                     <td>{data?.classTitle}</td>
                     <td>
                       <div className="flex items-center gap-3">
-                        <div className={`avatar border-4 rounded-2xl ${data?.status === "Pending" && "border-base-orange"} ${data?.status === "Accepted" && "border-green-500"} ${data?.status === "Rejected" && "border-red-500"}`}>
+                        <div
+                          className={`avatar border-4 rounded-2xl ${
+                            data?.status === "Pending" && "border-base-orange"
+                          } ${
+                            data?.status === "Accepted" && "border-green-500"
+                          } ${data?.status === "Rejected" && "border-red-500"}`}
+                        >
                           <div className="mask mask-squircle w-12 h-12">
                             <img
                               src={data?.classImage}
@@ -243,7 +268,20 @@ const MyClass = () => {
                     <td>
                       {data?.classDescription.split("").slice(0, 40)}.......
                     </td>
-                    <td className={`${data?.status === "Accepted" && "font-semibold text-green-500"} ${data?.status === "Pending" && "font-semibold text-blue-500"}${data?.status === "Rejected" && "font-semibold text-red-500"}`} >{data?.status}</td>
+                    <td
+                      className={`${
+                        data?.status === "Accepted" &&
+                        "font-semibold text-green-500"
+                      } ${
+                        data?.status === "Pending" &&
+                        "font-semibold text-blue-500"
+                      }${
+                        data?.status === "Rejected" &&
+                        "font-semibold text-red-500"
+                      }`}
+                    >
+                      {data?.status}
+                    </td>
                     <td>
                       <button
                         onClick={() => handleModalOpen(data)}
@@ -270,10 +308,9 @@ const MyClass = () => {
                             data?.status === "Rejected"
                           }
                           className={`p-2 rounded-lg  text-[#151515] ${
-                            data?.status === "Accepted" && "bg-green-500 hover:scale-95"
-                          } ${
-                            data?.status === "Rejected" && "bg-red-500"
-                          }`}
+                            data?.status === "Accepted" &&
+                            "bg-green-500 hover:scale-95"
+                          } ${data?.status === "Rejected" && "bg-red-500"}`}
                         >
                           <TbListDetails className="text-xl"></TbListDetails>
                         </button>
@@ -298,6 +335,67 @@ const MyClass = () => {
               loading={loading}
             ></UpdateModal>
           )}
+          <ol className="flex justify-center gap-1 text-xs font-medium mt-4">
+            <li>
+              <button
+                disabled={currentPage === 1}
+                onClick={() => handleCurrentPage(currentPage - 1)}
+                className="inline-flex size-8 items-center justify-center rounded border border-gray-100 bg-white text-gray-900 rtl:rotate-180"
+              >
+                <span className="sr-only">Prev Page</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-3 w-3"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
+            </li>
+
+            <li className="flex gap-2">
+              {pages.map((page, idx) => (
+                <button
+                  onClick={() => handleCurrentPage(page)}
+                  key={idx}
+                  className={`${
+                    currentPage === page
+                      ? "bg-base-orange text-black dark:text-white border-2 border-black"
+                      : "bg-white text-gray-900 dark:text-white"
+                  }block size-8 rounded   text-center leading-8 `}
+                >
+                  {page}
+                </button>
+              ))}
+            </li>
+
+            <li>
+              <button
+                disabled={currentPage === numberOfPages}
+                onClick={() => handleCurrentPage(currentPage + 1)}
+                className="inline-flex size-8 items-center justify-center rounded border border-gray-100 bg-white text-gray-900 rtl:rotate-180"
+              >
+                <span className="sr-only">Next Page</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-3 w-3"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
+            </li>
+          </ol>
         </div>
       </Container>
     </div>
